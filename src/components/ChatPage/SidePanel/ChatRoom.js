a5028627e6c10984d88acc30ef4f309f549398f1
billy5982 <kuu0404@naver.com>
2022-10-03 00:13:07 +0900

    ChatRoom 패널 구현 완료

--- src/components/ChatPage/SidePanel/ChatRoom.js
+++ src/components/ChatPage/SidePanel/ChatRoom.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { FaRegSmileWink } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
+import { BsFillTrashFill } from "react-icons/bs";
 import styled from "styled-components";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
@@ -16,7 +17,9 @@ import {
   update,
   child,
   DataSnapshot,
+  remove,
 } from "firebase/database";
+import { setCurrentChatRoom } from "../../../redux/actions/chatRoom_actions";
 import { async } from "@firebase/util";
 import { useEffect } from "react";
 const ChatRoomUIComponent = styled.div`
@@ -29,7 +32,11 @@ const ChatRoomUIComponent = styled.div`
 function ChatRoom() {
   const [show, setShow] = useState(false);
   const user = useSelector((state) => state.user.currentUser);
+  const dispatch = useDispatch();
   const [chatRoom, setChatRoom] = useState([]);
+  const [firstChatRoom, setFirstChatRoom] = useState(true);
+  //선택된 아이디의 효과를 주기 위한 상태관리
+  const [activeChatRoomId, setActiveChatRoomId] = useState("");
   // chatRoom 테이블의 주소를 가져오는것 ? 들을 가져오는 것?
   const chatRoomRef = ref(getDatabase(), "chatRoomRef");
 
@@ -103,23 +110,40 @@ function ChatRoom() {
     onChildAdded(chatRoomRef, (data) => {
       chatRoomsArray.push(data.val());
       setChatRoom([...chatRoomsArray]);
+      // 초기 진입 시 챗룸이 있으면 첫번째 챗룸으로 리듀서를 고정해줄 수 있다.
+      // 챗룸이 없으면 리듀서의 상태를 그냥 null 상태로 배치
+      if (firstChatRoom && chatRoomsArray.length > 0) {
+        //처음 룸이 선택되있게 고정
+        dispatch(setCurrentChatRoom(chatRoomsArray[0]));
+        setFirstChatRoom(false);
+        setActiveChatRoomId(chatRoomsArray[0].id);
+      }
     });
   };
+
   useEffect(() => {
     AddChatRoomsListeners();
   }, []);
+
   // 간단한 유효성 검사 해당
   const isFormValid = (nam, des) => nam && des;
 
-  // li (채팅방) 클릭시 발생 이벤트 -> 화면 전환 이벤트
+  // li (채팅방) 클릭시 발생 이벤트 -> 화면 전환 이벤트 -> 현재 리듀서에 클릭한 방의 정보를 담아주고
+  // mainpannel에서 해당 방에 대한 정보를 뿌려줘야 함.
   const changeChatRoom = (room) => {
+    dispatch(setCurrentChatRoom(room));
+    // 클릭한 room의 아이디로
+    setActiveChatRoomId(room.id);
+  };
+  // 업데이트를 이용하여 해당 데이터 삭제?
+  const removeChatRoom = (room) => {
     console.log(room);
   };
   return (
     <div>
       <ChatRoomUIComponent>
         <FaRegSmileWink style={{ marginRight: 3 }} />
-        CHAT ROOMS (1)
+        CHAT ROOMS {chatRoom.length}
         <FaPlus
           onClick={onClick}
           style={{ position: "absolute", right: 0, cursor: "pointer" }}
@@ -129,8 +153,19 @@ function ChatRoom() {
         {chatRoom.length !== 0
           ? chatRoom.map((room) => (
               // 클릭한 정보를 리덕스에다가 넣어줘야함
-              <li onClick={() => changeChatRoom(room)} key={room.id}>
+              <li
+                onClick={() => changeChatRoom(room)}
+                key={room.id}
+                style={{
+                  backgroundColor: room.id === activeChatRoomId && "#ffffff45",
+                  display: "flex",
+                }}
+              >
                 # {room.name}
+                <BsFillTrashFill
+                  style={{ cursor: "pointer" }}
+                  onClick={() => removeChatRoom(room)}
+                />
               </li>
             ))
           : null}
