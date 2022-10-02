20f2a87e69615aaeac68ddbb0d3ee78aefd22a95
billy5982 <kuu0404@naver.com>
2022-10-03 00:14:29 +0900

    reducer 추가

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,11 +1,12 @@
 // Reducer을 합쳐주기 위해 불러옴
 import { combineReducers } from "redux";
 import user from "./user_reducer";
+import chatRoom from "./chatRoom_reducer";
 // import chatRoom from './chatRoom_reducer'
 
 const rootReducer = combineReducers({
   user,
-  // chatRoom
+  chatRoom,
 });
 
 export default rootReducer;
