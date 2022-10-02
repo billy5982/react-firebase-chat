51522348af966838e89326959b7ccad93b283e47
billy5982 <kuu0404@naver.com>
2022-10-03 00:15:34 +0900

    reducer action 추가

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -1,3 +1,5 @@
 export const SET_USER = "set_user";
 export const CLEAR_USER = "clear_user";
 export const SET_PHOTOURL = "set_photoURL";
+
+export const SET_CURRENT_CHAT_ROOM = "set_current_chat_room";
