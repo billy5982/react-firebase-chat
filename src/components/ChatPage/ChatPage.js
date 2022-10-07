8b9c76d51debd24189f91a6e01ca25bc8f170ea8
billy5982 <kuu0404@naver.com>
2022-10-08 01:16:50 +0900

    favorited을 위한 컴포넌트 제작

--- src/components/ChatPage/ChatPage.js
+++ src/components/ChatPage/ChatPage.js
@@ -1,17 +1,29 @@
-import React from "react";
 import SidePanel from "./SidePanel/SidePanel";
 import MainPanel from "./MainPanel/MainPanel";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+
 function ChatPage() {
   const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
-
+  const currentUser = useSelector((state) => state.user.currentChatRoom);
+  const [heart, setHeart] = useState(false);
   return (
     <div style={{ display: "flex" }}>
       <div style={{ width: "300px" }}>
-        <SidePanel />
+        <SidePanel
+          key={currentUser && currentUser.uid}
+          heart={heart}
+          setHeart={setHeart}
+        />
       </div>
       <div style={{ width: "100%" }}>
-        {chatRoom && <MainPanel key={chatRoom && chatRoom.id} />}
+        {chatRoom && (
+          <MainPanel
+            key={chatRoom && chatRoom.id}
+            heart={heart}
+            setHeart={setHeart}
+          />
+        )}
       </div>
     </div>
   );
